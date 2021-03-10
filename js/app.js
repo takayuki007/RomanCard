f4f88cb1c9c9cb09209f8d05da9539d6e603cf12
TAKAYUKI MIYOSHI <pico.pico.hammer@icloud.com>
2021-03-10 20:45:08 +0900

    ・環境構築 ・HTMLの完成 ・PC、TAB、SPのCSS完成 ・jQueryの完成

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// フェードインイベント
+$(window).scroll(function (){
+    $('.js-fadein').each(function (){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('js-scrollin');
+        }
+    });
+});
